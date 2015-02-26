a207817a0b1b2b209e76e6f8d028df0845a720c0
Max Grechko <max@grechko.org>
2015-02-26 14:15:04 +0200

    Add a 'new project' form.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,23 @@
 class ProjectsController < ApplicationController
   def index
     @projects = Project.all
+    @task = Task.new
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to projects_path
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root to: 'projects#index'
+  resources :projects
+  resources :tasks
 end
