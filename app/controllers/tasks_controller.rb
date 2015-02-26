2059b287efc030cebbf33d3630bb76e453e643a6
Max Grechko <max@grechko.org>
2015-02-26 21:27:27 +0200

    Add Ajax for check and delete actions.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,9 +13,26 @@ class TasksController < ApplicationController
     end
   end
 
+  def update
+    @task = Task.find_by_id(params[:id])
+    @task.update_attributes(task_params)
+    respond_to do |format|
+      format.html { redirect_to projects_path }
+      format.js
+    end
+  end
+
+  def destroy
+    @task = Task.destroy(params[:id])
+    respond_to do |format|
+      format.html { redirect_to projects_path }
+      format.js
+    end
+  end
+
   private
 
   def task_params
-    params.require(:task).permit(:name)
+    params.require(:task).permit(:name, :done)
   end
 end
