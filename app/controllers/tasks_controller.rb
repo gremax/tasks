cd96994b5188b8b4fe16b5edce0e611f2841e585
Max Grechko <max@grechko.org>
2015-02-26 15:21:03 +0200

    Add a 'new task' function.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,21 @@
 class TasksController < ApplicationController
+  def new
+  end
+
   def create
+    @task = Task.new(task_params)
+    @task.project_id = params[:project_id]
+    if @task.save
+      redirect_to projects_path
+    else
+      flash[:warning] = "The form can't be blank."
+      redirect_to projects_path
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name)
   end
 end
