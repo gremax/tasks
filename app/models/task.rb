03bd4620acbbb15eea079490e8b30f701cbe394d
Max Grechko <max@grechko.org>
2015-02-27 11:05:25 +0200

    Add a complete list for tasks.

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,12 @@ class Task < ActiveRecord::Base
   validates :name, presence: true
 
   belongs_to :project
+
+  def self.incomplete
+    where(done: false)
+  end
+
+  def self.complete
+    where(done: true)
+  end
 end
